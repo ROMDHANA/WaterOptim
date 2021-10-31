364922fdc6e158f55a5958b502912f16970dec4e
ROMDHANA HEDI <50572070+ROMDHANA@users.noreply.github.com>
2021-10-31 02:39:44 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'WaterOptim'
+copyright = '2021, Romdhana'
+author = 'Romdhana'
 
 release = '0.1'
 version = '0.1.0'
